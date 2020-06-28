0ca898d58bb8d41db130b2e58ff24492949c4eda
Zoma <hazem_ahmed_93@yahoo.com>
2020-06-28 10:13:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
